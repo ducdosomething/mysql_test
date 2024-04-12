eda5ba0891f08647c1b8880484e42585cc789708
ducdosomething <duclehoang211201@gmail.com>
2024-04-12 16:32:03 +0700

    demo mySQL code yesterday

--- demo_product_manager.sql
+++ demo_product_manager.sql
@@ -45,3 +45,56 @@ false,
 "2014-1-1"
 );
 
+insert into product
+value (
+"PR004",
+"Iphone 14",
+"The iPhone 14 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 5.85 inches diagonally (actual viewable area is less).",
+"https://cdsassets.apple.com/live/SZLF0YNV/images/sp/111864_iphonex.png",
+false,
+50000000,
+"2014-1-1",
+"64gb"
+);
+
+insert into product
+value (
+"PR005",
+"Iphone 14 pro",
+"The iPhone 14 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 5.85 inches diagonally (actual viewable area is less).",
+"https://cdsassets.apple.com/live/SZLF0YNV/images/sp/111864_iphonex.png",
+false,
+50000000,
+"2014-1-1",
+"1TB"
+);
+
+insert into product
+value (
+"PR0006",
+"Iphone 15 pro max",
+"The iPhone 15 pro max display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 5.85 inches diagonally (actual viewable area is less).",
+"https://cdsassets.apple.com/live/SZLF0YNV/images/sp/111864_iphonex.png",
+"false",
+50000000,
+"2014-1-1"
+);
+
+insert into product
+value (
+"PR7",
+"Iphone 7",
+"The iPhone 7 display has rounded corners that follow a beautiful curved design, and these corners are within a standard rectangle. When measured as a standard rectangular shape, the screen is 5.85 inches diagonally (actual viewable area is less).",
+"https://cdsassets.apple.com/live/SZLF0YNV/images/sp/111864_iphonex.png",
+1,
+5000000,
+"2019-11-11",
+"256gb"
+);
+
+alter table product
+add column version enum("64gb", "128gb", "256gb");
+
+
+
+
